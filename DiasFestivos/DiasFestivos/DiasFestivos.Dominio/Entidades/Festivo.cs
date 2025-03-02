10851a48bf2b879eb1900f2ba309bb3182bfe0cd
AlanMJ02q <alan.monroy898@pascualbravo.edu.co>
2025-03-02 17:40:26 -0500

    Add files via upload

---
+++ DiasFestivos/DiasFestivos/DiasFestivos.Dominio/Entidades/Festivo.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DiasFestivos.Dominio.Entidades
+{
+    [Table("Festivo")]
+    public class Festivo
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Nombre"), StringLength(100)]
+        public required string Nombre { get; set; }
+
+        [Column("Dia")]
+        public int Dia { get; set; }
+
+        [Column("Mes"), StringLength(100)]
+        public required string Mes { get; set; }
+
+        [Column("DiasPascua")]
+        public int DiasPascua { get; set; }
+
+        [Column("IdTipo")]
+        public int IdTipo { get; set; }
+
+
+
+    }
+}
---
+++ DiasFestivos/DiasFestivos/DiasFestivos.Dominio/Entidades/Tipo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DiasFestivos.Dominio.Entidades
+{
+    [Table("Tipo")]
+    public class Tipo
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Tipo")]
+        public int Tipos { get; set; }
+
+    }
+}
